c240d9d97d4090bd669f0e3e3309b959920e6c1f
Matthew Soulanille <msoulanille@google.com>
2021-01-05 13:55:37 -0800

    Subtle crypto is only available on https

--- browserstack-file-size/src/test.ts
+++ browserstack-file-size/src/test.ts
@@ -1,24 +1,19 @@
-async function sha256(buf: ArrayBuffer): Promise<string> {
-  const shaBytes = await crypto.subtle.digest('SHA-256', buf);
-  return [...(new Uint8Array(shaBytes))]
-    .map(num => num.toString(16))
-    .join('');
-}
+import {sha256} from 'js-sha256';
 
 describe('file size test', () => {
   let timeout: number;
   beforeAll(() => {
     timeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
   });
 
   afterAll(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout;
-  })
+  });
 
   it('loads a large file', async () => {
     const response = await fetch('base/src/random.bin');
-    const sha = await sha256(await response.arrayBuffer());
+    const sha = sha256(await response.arrayBuffer());
     // tslint:disable-next-line max-line-length
     expect(sha).toEqual('4366976e797721ec77ddcf3d1c266b98754ef5cf6de328e56847cea6e1a8da2f');
   });
