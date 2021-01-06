3c1bac1c0a30f3aca47c47b94e4966d32b866084
Matthew Soulanille <msoulanille@google.com>
2021-01-06 14:14:37 -0800

    Add a timestamp to the file test logs

--- browserstack-file-size/src/file_size_test.ts
+++ browserstack-file-size/src/file_size_test.ts
@@ -1,5 +1,10 @@
 import {sha256} from 'js-sha256';
 
+function timestamp() {
+  const date = new Date();
+  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds}.${date.getMilliseconds()}`;
+}
+
 describe('file size test', () => {
   let timeout: number;
   beforeAll(() => {
@@ -12,13 +17,13 @@ describe('file size test', () => {
   });
 
   it('loads a large file', async () => {
-    console.log('Fetching file');
+    console.log(`${timestamp()}: Fetching file`);
     const response = await fetch('base/src/random.bin');
-    console.log('Got file. Calculating sha256');
+    console.log(`${timestamp()}: Got file. Calculating sha256`);
     const sha = sha256(await response.arrayBuffer());
-    console.log(`Calculated sha256 is ${sha}`);
+    console.log(`${timestamp()}: Calculated sha256 is ${sha}`);
     // tslint:disable-next-line max-line-length
     expect(sha).toEqual('4366976e797721ec77ddcf3d1c266b98754ef5cf6de328e56847cea6e1a8da2f');
-    console.log('Checked calculated sha256 with expected value');
+    console.log(`${timestamp()}: Checked calculated sha256 with expected value`);
   });
 });
