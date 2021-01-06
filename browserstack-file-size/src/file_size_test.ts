965389618c61c86a425df124f7ab9c0001912d39
Matthew Soulanille <msoulanille@google.com>
2021-01-06 14:23:45 -0800

    Pad the date formats

--- browserstack-file-size/src/file_size_test.ts
+++ browserstack-file-size/src/file_size_test.ts
@@ -2,7 +2,11 @@ import {sha256} from 'js-sha256';
 
 function timestamp() {
   const date = new Date();
-  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`;
+  const ms = date.getMilliseconds().toString().padStart(3, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}.${ms}`;
 }
 
 describe('file size test', () => {
