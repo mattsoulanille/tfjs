40cf710fc5474efc1f06961651a1e07891cc8054
Matthew Soulanille <msoulanille@google.com>
2021-01-07 10:50:10 -0800

    Log how much of the file has loaded

--- browserstack-file-size/src/file_size_test.ts
+++ browserstack-file-size/src/file_size_test.ts
@@ -9,13 +9,50 @@ function timestamp() {
   return `${hours}:${minutes}:${seconds}.${ms}`;
 }
 
-async function delay(ms: number): Promise<void> {
-  await new Promise(fulfill => {
-    setTimeout(fulfill, ms);
-  });
+// async function delay(ms: number): Promise<void> {
+//   await new Promise(fulfill => {
+//     setTimeout(fulfill, ms);
+//   });
+// }
+
+//const TIMEOUT = 60000 * 4; // 4 minutes
+
+async function fetchWithLogs(url: string, logPeriod = 1000,
+                             log=console.log): Promise<Uint8Array> {
+
+  const response = await fetch(url);
+  const reader = response.body.getReader();
+
+  const chunks: Uint8Array[] = [];
+  let receivedLength = 0;
+
+  const logInterval = setInterval(() => {
+    log(`${timestamp()}: Received ${receivedLength} bytes`);
+  }, logPeriod);
+
+  while (true) {
+    const {done, value} = await reader.read();
+    if (done) {
+      break;
+    }
+
+    chunks.push(value);
+    receivedLength += value.length;
+
+  }
+
+  clearInterval(logInterval);
+
+  const outBuf = new Uint8Array(receivedLength);
+  let position = 0;
+  for (const chunk of chunks) {
+    outBuf.set(chunk, position);
+    position += chunk.length;
+  }
+
+  return outBuf;
 }
 
-const TIMEOUT = 60000 * 4; // 4 minutes
 
 describe('file size test', () => {
   let timeout: number;
@@ -30,17 +67,9 @@ describe('file size test', () => {
 
   it('loads a large file', async () => {
     console.log(`${timestamp()}: Fetching file`);
-    const responseOrTimeout = await Promise.race([
-      fetch('base/src/random.bin').then(response => response.arrayBuffer()),
-      delay(TIMEOUT)
-    ]);
-
-    if (!responseOrTimeout) {
-      throw new Error(`${timestamp()}: Fetch timed out`);
-    }
-
+    const data = await fetchWithLogs('base/src/random.bin');
     console.log(`${timestamp()}: Got file. Calculating sha256`);
-    const sha = sha256(responseOrTimeout);
+    const sha = sha256(data);
     console.log(`${timestamp()}: Calculated sha256 is ${sha}`);
     // tslint:disable-next-line max-line-length
     expect(sha).toEqual('4366976e797721ec77ddcf3d1c266b98754ef5cf6de328e56847cea6e1a8da2f');
