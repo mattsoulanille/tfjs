708ecd77cada5a48a188c850b6e13c7cfcd2717b
Matthew Soulanille <msoulanille@google.com>
2022-02-02 14:34:37 -0800

    [webgl] Move index.ts to root of core package

--- tfjs-backend-webgl/src/setup_test.ts
+++ tfjs-backend-webgl/src/setup_test.ts
@@ -17,7 +17,7 @@
 
 import '@tensorflow/tfjs-backend-cpu';
 // Register the backend.
-import './index';
+import '../index';
 // tslint:disable-next-line: no-imports-from-dist
 import '@tensorflow/tfjs-core/dist/public/chained_ops/register_all_chained_ops';
 // tslint:disable-next-line: no-imports-from-dist
